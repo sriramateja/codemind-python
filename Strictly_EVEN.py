5df34ee224f7a68aab637812e2579ce53698a38b
sriramateja <109516518+sriramateja@users.noreply.github.com>
2022-10-10 15:00:15 +0530

    a=input() b=list(map(int,input().split())) c=True d=-1 for i in b:     d+=1     if i%2==0:         if d%2==0:             c=True         else:             c=False             break print(c)

---
+++ Strictly_EVEN.py
@@ -0,0 +1,14 @@
+a=input()
+b=list(map(int,input().split()))
+c=True
+d=-1
+for i in b:
+    d+=1
+    if i%2==0:
+        if d%2==0:
+            c=True
+        else:
+            c=False
+            break
+print(c)
+    
