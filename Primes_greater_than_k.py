402fdbb422d37dd44dfc04eb5d0a4620e1622dab
sriramateja <109516518+sriramateja@users.noreply.github.com>
2022-10-23 13:25:00 +0530

    def prime(x):     if x<2:return False     for i in range(2,x//2+1):         if x%i==0:return False     return True a=int(input()) b=list(map(int,input().split())) c=int(input()) d=0 for i in b:     if i>=c:         if prime(i):d+=1 print(d)

---
+++ Primes_greater_than_k.py
@@ -0,0 +1,14 @@
+def prime(x):
+    if x<2:return False
+    for i in range(2,x//2+1):
+        if x%i==0:return False
+    return True
+a=int(input())
+b=list(map(int,input().split()))
+c=int(input())
+d=0
+for i in b:
+    if i>=c:
+        if prime(i):d+=1
+print(d)
+
