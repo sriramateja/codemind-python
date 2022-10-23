5808945f48557b180319b02f17750ca9ca4b1da1
sriramateja <109516518+sriramateja@users.noreply.github.com>
2022-10-23 12:45:26 +0530

    a,b=map(int,input().split()) c=[] for i in range(1,b//2+1):     if b%i==0:         c+=[i] for i in c[::-1]:     if a%i==0:         gcd=i         break print(gcd)

--- GCD_or_HCF.py
+++ GCD_or_HCF.py
@@ -1,10 +1,10 @@
 a,b=map(int,input().split())
-x=[]
-for i in range(1,a+1):
-    if a%i==0:
-        x+=[i]
-c=x[::-1]
-for i in c:
+c=[]
+for i in range(1,b//2+1):
     if b%i==0:
-        print(i)
+        c+=[i]
+for i in c[::-1]:
+    if a%i==0:
+        gcd=i
         break
+print(gcd)
