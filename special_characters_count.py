94a0e97a8a6932ad1b2e3d8bbc202d97e2fb65c9
sriramateja <109516518+sriramateja@users.noreply.github.com>
2022-10-26 18:42:07 +0530

    #65 90 97 122 a=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' '] b=input() cnt=0 for i in b:     if i not in a:cnt+=1 print(cnt)

---
+++ special_characters_count.py
@@ -0,0 +1,7 @@
+#65 90 97 122
+a=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',' ']
+b=input()
+cnt=0
+for i in b:
+    if i not in a:cnt+=1
+print(cnt)
