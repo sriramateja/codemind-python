364f752bdf39472405ddc4e0df1bcdad2cd801f1
sriramateja <109516518+sriramateja@users.noreply.github.com>
2022-10-28 18:37:19 +0530

    a=input().lower() b=input().lower() c=[] if len(a)>len(b):     x=a     y=b else:     x=b     y=a for i in x:     if i!=' ':         if i not in y  and i not in c:c+=[i]
    for i in y:
        if i!=' ':
            if i not in x  and i not in c:c+=[i]
    e=(sorted(c))
    print(''.join(e))

---
+++ non_common_characters.py
@@ -0,0 +1,18 @@
+a=input().lower()
+b=input().lower()
+c=[]
+if len(a)>len(b):
+    x=a
+    y=b
+else:
+    x=b
+    y=a
+for i in x:
+    if i!=' ':
+        if i not in y  and i not in c:c+=[i]
+
+for i in y:
+    if i!=' ':
+        if i not in x  and i not in c:c+=[i]
+e=(sorted(c))
+print(''.join(e))
